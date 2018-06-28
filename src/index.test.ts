10e1dc5a1a1c9e7a45b7f7c3ad84e77b28fe5f87
Philipp Kyeck <philipp@beta-interactive.de>
2018-06-28 18:00:39 +0200

    use credentials instead of keyFile
    and remove config via ENV vars

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,16 +7,15 @@ describe('multer-google-storage', () => {
         mockery.enable();
         const storageMock = () => { return {bucket: () => {}}};
         mockery.registerMock('@google-cloud/storage', storageMock);
-
-        process.env.GCS_BUCKET = 'test';
-        process.env.GCLOUD_PROJECT = 'test';
-        process.env.GCS_KEYFILE = './test';
     });
 
     it('should define multer storage engine interface',() => {
 
         const MulterGoogleCloudStorage = require('./index').default;
-        const cloudStorage = new MulterGoogleCloudStorage();
+        const cloudStorage = new MulterGoogleCloudStorage({
+            bucket: 'test',
+            credentials: {},
+        });
 
         expect(cloudStorage._handleFile).to.be.a('function');
         expect(cloudStorage._removeFile).to.be.a('function');
