4d2d8e8436f4e81b00a1cc3f9fcc9de4fe093a8f
Philipp Kyeck <philipp@beta-interactive.de>
2018-06-29 11:33:07 +0200

    add debug logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import * as multer from 'multer';
 import Promise from 'bluebird';
+import _debug from 'debug';
 import { Storage, Bucket, ConfigurationObject } from '@google-cloud/storage';
 import * as uuid from 'uuid/v1';
-const storage: (options?: ConfigurationObject) => Storage = require('@google-cloud/storage');
 
+const storage: (options?: ConfigurationObject) => Storage = require('@google-cloud/storage');
+const debug = _debug('multergcs');
+
+//
+//
 export default class MulterGoogleCloudStorage implements multer.StorageEngine {
   private gcobj: Storage;
   private gcsBucket: Bucket;
@@ -39,15 +44,21 @@ export default class MulterGoogleCloudStorage implements multer.StorageEngine {
   }
 
   _handleFile = (req, file, cb) => {
+    debug('_handleFile()', file);
     this.getDestination(req, file, (err, destination) => {
       if (err) {
+        debug('_handleFile() > getDestination > error', err);
         return cb(err);
       }
 
+      debug('_handleFile() > getDestination', destination);
+
       this.getFilename(req, file, (err, filename) => {
         if (err) {
+          debug('_handleFile() > getFilename > error', err);
           return cb(err);
         }
+        debug('_handleFile() > getFilename', filename);
         var gcFile = this.gcsBucket.file(filename);
         file.stream
           .pipe(
@@ -55,22 +66,28 @@ export default class MulterGoogleCloudStorage implements multer.StorageEngine {
               predefinedAcl: this.options.acl || 'private',
             })
           )
-          .on('error', err => cb(err))
-          .on('finish', file =>
+          .on('error', err => {
+            debug('_handleFile() > error', err);
+            cb(err);
+          })
+          .on('finish', file => {
+            debug('_handleFile() > finish');
             cb(null, {
               path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
               filename: filename,
-            })
-          );
+            });
+          });
       });
     });
   };
   _removeFile = (req, file, cb) => {
-    var gcFile = this.gcsBucket.file(file.filename);
+    debug('_removeFile()');
+    const gcFile = this.gcsBucket.file(file.filename);
     gcFile.delete();
   };
 }
 
 export function storageEngine(opts?: ConfigurationObject & { filename?: any; bucket?: string }) {
+  debug('init storageEngine Multer GCS ...', opts);
   return new MulterGoogleCloudStorage(opts);
 }
